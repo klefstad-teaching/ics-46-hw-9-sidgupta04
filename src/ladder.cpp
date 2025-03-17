8beb838385a8eb763134b5454a625d2a65fb43ea
Siddharth Gupta <emailsiddharthgupta@gmail.com>
2025-03-17 00:16:30 +0000

    forgot a semicolon

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -96,7 +96,7 @@ void print_word_ladder(const vector<string>& ladder) {
     if (ladder.empty()) {
         cout << "No word ladder found." << endl;
     } else {
-        cout << "Word ladder found: "
+        cout << "Word ladder found: ";
         for (size_t i = 0; i < ladder.size(); ++i) {
             cout << ladder[i] << " ";
         }
